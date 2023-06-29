6babd4dc2b198243fc305c1a3ace90c086dd1cae
RAMANANJATOVO JOHNNY <ramananmichel@gmail.com>
2023-06-29 15:54:41 +0200

    Modification ACcueil

--- Front_end/Teste-Front-End---Healthy-Food-main/src/components/main/hero/HeroSection.jsx
+++ Front_end/Teste-Front-End---Healthy-Food-main/src/components/main/hero/HeroSection.jsx
@@ -8,12 +8,35 @@ export const Hero = () => {
   const [height, setHeight] = useState("");
   const [weight, setWeight] = useState("");
   const [bmi, setBmi] = useState(null);
+  const [bmiCategory, setBmiCategory] = useState("");
+  const [error, setError] = useState(null);
 
   const calculateBMI = () => {
     if (height !== "" && weight !== "") {
-      const heightInMeters = height / 100;
-      const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
-      setBmi(bmi);
+      if (height < 100 || height > 250 || weight < 20 || weight > 300) {
+        setError("Please enter valid values for height (100-250cm) and weight (20-300kg)");
+        setBmi(null);
+        setBmiCategory("");
+      } else {
+        const heightInMeters = height / 100;
+        const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+        setBmi(bmi);
+        setError(null);
+
+        if (bmi < 18.5) {
+          setBmiCategory("Inférieur au poids normal");
+        } else if (bmi < 25) {
+          setBmiCategory("Poids normal");
+        } else if (bmi < 30) {
+          setBmiCategory("En surpoids");
+        } else if (bmi < 35) {
+          setBmiCategory("Obésité de classe I");
+        } else if (bmi < 40) {
+          setBmiCategory("Obésité de classe II");
+        } else {
+          setBmiCategory("Obésité de classe III");
+        }
+      }
     }
   };
 
@@ -25,19 +48,25 @@ export const Hero = () => {
           <div className="handle">
             <input
               type="number"
-              placeholder="Enter your height in cm"
+              placeholder="Your height in cm"
               value={height}
               onChange={(e) => setHeight(e.target.value)}
             />
             <input
               type="number"
-              placeholder="Enter your weight in kg"
+              placeholder="Your weight in kg"
               value={weight}
               onChange={(e) => setWeight(e.target.value)}
             />
             <button type="button" onClick={calculateBMI}><p>Calculate </p></button>
-            {bmi && <p>Your BMI is: {bmi}</p>}
+            {error && <p className="error">{error}</p>}
           </div>
+          {bmi && (
+            <>
+              <h3>Your BMI is: {bmi}</h3>
+              <h3> - - - Category: {bmiCategory}</h3>
+            </>
+          )}
         </div>
 
         <div className="rigthContent">
